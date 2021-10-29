7bca9878d259da56ee0be4df2873db8acc279b67
korede <koredebada@gmail.com>
2021-10-29 12:53:38 +0100

    checking error

--- public/js/app.js
+++ public/js/app.js
@@ -2,14 +2,18 @@
  const select = document.querySelector(".flex-form select");
  const numberform = document.querySelector(".numberform");
  const ccode = document.querySelector("#code");
+ const ng = document.querySelector(".ng");
+//  const otp = document.querySelectorAll(".otp input");
        
 
  fetch("https://countriesnow.space/api/v0.1/countries/flag/unicode", requestOptions)
  .then(response => response.json())
  .then(result => {
+     ng.innerHTML = result.data[140].unicodeFlag
+     ng.value = result.data[140].name
      
-     select.innerHTML = result.data.map(e => `
-     <option selected value=${e.name}>
+     select.innerHTML += result.data.map((e,i) => `
+     <option value=${e.name}>
      ${e.unicodeFlag}
      </option>
      `).join("")
@@ -38,7 +42,9 @@
             })
             console.log(code)
             ccode.value = code[0].dial_code
+            
         })
         .catch(error => console.log('error', error));
      
  })
+
